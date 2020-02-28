6e99f5410e4569bbd528ccedb8b7f798e700e2dd
Brayan Tabares <brayantabares10a2014@gmail.com>
2020-02-28 14:57:12 -0500

    Impletemented Kodein

--- app/src/main/java/com/example/forecastmvvm/ForecastApplication.kt
+++ app/src/main/java/com/example/forecastmvvm/ForecastApplication.kt
@@ -1,12 +1,26 @@
 package com.example.forecastmvvm
 
 import android.app.Application
+import com.example.forecastmvvm.Data.db.ForecastDatabase
+import com.example.forecastmvvm.Data.network.*
+import com.example.forecastmvvm.Data.repository.ForecastRepository
+import com.example.forecastmvvm.Data.repository.ForecastRepositoryImpl
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
 
 class ForecastApplication : Application(), KodeinAware {
     override val kodein = Kodein.lazy {
         import(androidXModule(this@ForecastApplication))
+
+        bind() from singleton { ForecastDatabase(instance()) }
+        bind() from singleton { instance<ForecastDatabase>().currentWeatherDao() }
+        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
+        bind() from singleton { ApixuWeatherApiService(instance()) }
+        bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
+        bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(), instance()) }
     }
 }
